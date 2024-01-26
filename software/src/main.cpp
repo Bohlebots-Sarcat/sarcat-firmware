f2adf5b15430ffdc696ea050c86750d19ed280f5
maupwastaken <78-maup@protonmail.com>
2024-01-26 11:57:22 +0100

    implemented new strategy

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,25 +1,19 @@
 #include "GameModes.h"
-#include "BohleBots.h"
 
 GameModes mode;
 
 void setup() {
     mode.init();
     mode.isStandby = true;
-    mode.lightOn = false;
-    mode.isPlaying = false;
 }
 
 void loop() {
     BohleBots bot;
 
-    if (bot.boardTaster(1)) {
-        mode.isPlaying = true;
-    }
+    if (bot.boardTaster(1)) mode.toggle(1);
+    if (bot.boardTaster(2)) mode.toggle(2);
 
-    if (mode.isPlaying) { mode.play(); }
-
-    Serial.println(bot.ballRichtung());
+    mode.run();
 
     bot.warte(5);
 }
